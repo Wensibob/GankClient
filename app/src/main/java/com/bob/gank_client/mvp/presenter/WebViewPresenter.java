b375631f11c25a351260a1f940a0d5d294eae80a
Wensibob <Wensibob@gmail.com>
2017-05-10 22:42:53 +0800

    add a web presenter and fix some layout bugs

--- app/src/main/java/com/bob/gank_client/mvp/presenter/WebViewPresenter.java
+++ app/src/main/java/com/bob/gank_client/mvp/presenter/WebViewPresenter.java
@@ -1,14 +1,29 @@
 package com.bob.gank_client.mvp.presenter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import com.bob.gank_client.R;
+import com.bob.gank_client.mvp.model.entity.Gank;
 import com.bob.gank_client.mvp.view.IWebView;
+import com.bob.gank_client.utils.APPUtil;
+import com.bob.gank_client.utils.ShareUtil;
+
+import java.net.URI;
 
 /**
  * Created by bob on 17-5-1.
  */
 
 public class WebViewPresenter extends BasePresenter<IWebView> {
+        //TODO 添加类似Chrome的效果，将网页以及视频播放使用同一个Presenter
 
         public WebViewPresenter(Context context, IWebView iView) {
                 super(context, iView);
@@ -16,8 +31,71 @@ public class WebViewPresenter extends BasePresenter<IWebView> {
 
         @Override
         public void release() {
-
         }
 
-        //TODO 添加类似Chrome的效果，将网页以及视频播放使用同一个Presenter
+        public void setWebViewSettings(WebView webView, String url) {
+                WebSettings settings = webView.getSettings();
+                settings.setJavaScriptEnabled(true);
+                settings.setLoadWithOverviewMode(true);
+                settings.setAppCacheEnabled(true);
+                settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+                settings.setSupportZoom(true);
+                webView.setWebChromeClient(new ChromeClient());
+                webView.setWebViewClient(new GankWebClient());
+                webView.loadUrl(url);
+        }
+
+        public void refresh(WebView webView) {
+                webView.reload();
+        }
+
+        public void copyUrl(View view, String text) {
+                APPUtil.copyToClipboard(view,text, context.getString(R.string.copy_successed));
+        }
+
+        public void openInBrowser(String url) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse(url);
+                intent.setData(uri);
+                if (intent.resolveActivity(context.getPackageManager()) != null) {
+                        context.startActivity(intent);
+                } else {
+                        iView.openFailed();
+                }
+        }
+
+        public void moreOperation(Gank gank) {
+                if (gank != null) {
+                        ShareUtil.shareGank(context,gank);
+                }
+        }
+
+
+        private class ChromeClient extends WebChromeClient {
+                @Override
+                public void onProgressChanged(WebView view, int newProgress) {
+                        super.onProgressChanged(view, newProgress);
+                        iView.showProgress(newProgress);
+                }
+
+                @Override
+                public void onReceivedTitle(WebView view, String title) {
+                        super.onReceivedTitle(view, title);
+                        iView.setWebTitle(title);
+                }
+        }
+
+        private class GankWebClient extends WebViewClient {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url ) {
+                        if (url != null) {
+                                view.loadUrl(url);
+                        }
+                        return true;
+                }
+        }
+
+
 }
+
--- app/src/main/java/com/bob/gank_client/utils/ShareUtil.java
+++ app/src/main/java/com/bob/gank_client/utils/ShareUtil.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
+import com.bob.gank_client.R;
+import com.bob.gank_client.mvp.model.entity.Gank;
+
 /**
  * Created by bob on 17-5-1.
  */
@@ -18,6 +21,14 @@ public class ShareUtil {
                 context.startActivity(Intent.createChooser(shareIntent, title));
         }
 
+        public static void shareGank(Context context, Gank gank) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, gank.desc + gank.url );
+                intent.setType("text/plain");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(Intent.createChooser(intent, context.getString(R.string.share_gank_to)));
+        }
         //TODO 分享图片如上，分享链接的时候需要做到如简书的效果
 
         //TODO 分享APP的时候可以适当花些功夫
